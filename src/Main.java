98136e103b427b99cd58e2a9044eabba78db2516
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-20 20:16:41 +0530

    UC 2 Handling Floats

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static Integer findMax(Integer a, Integer b, Integer c) {
-        Integer max = a; // Assume a is the largest
+    public static <T extends Comparable<T>> T findMax(T a, T b, T c) {
+        T max = a; // Assume a is the largest
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -11,8 +11,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Max (1st): " + findMax(9, 3, 1)); // Max in 1st position
-        System.out.println("Max (2nd): " + findMax(2, 9, 1)); // Max in 2nd position
-        System.out.println("Max (3rd): " + findMax(1, 3, 9)); // Max in 3rd position
+        System.out.println("Max Integer (1st): " + findMax(9, 3, 1));
+        System.out.println("Max Float (2nd): " + findMax(2.2f, 9.5f, 1.3f));
     }
 }
