b051a620d888eaf876c42778fb3f59574d289db3
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-20 21:34:39 +0530

    Refactored the code to include all three in one

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-public class Main {
+public class Main<T extends Comparable<T>> {
+    private T a, b, c;
+
+    public Main(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T findMax() {
+        return findMax(a, b, c);
+    }
+
     public static <T extends Comparable<T>> T findMax(T a, T b, T c) {
         T max = a; // Assume a is the largest
         if (b.compareTo(max) > 0) {
@@ -11,8 +23,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Max Integer (1st): " + findMax(9, 3, 1));
-        System.out.println("Max Float (2nd): " + findMax(2.2f, 9.5f, 1.3f));
-        System.out.println("Max String (3rd): " + findMax("Apple", "Peach", "Banana"));
+        Main<Integer> intMax = new Main<>(9, 3, 1);
+        System.out.println("Max Integer: " + intMax.findMax());
+
+        Main<Float> floatMax = new Main<>(2.2f, 9.5f, 1.3f);
+        System.out.println("Max Float: " + floatMax.findMax());
+
+        Main<String> stringMax = new Main<>("Apple", "Peach", "Banana");
+        System.out.println("Max String: " + stringMax.findMax());
     }
 }
+
